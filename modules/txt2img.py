9b016415e23b30e1ce5e72fe39c727374ccf052c
minhnhatdo <minhnhatdo0709@gmail.com>
2023-09-15 23:35:38 +0700

    fix account load

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -59,7 +59,14 @@ def batch_txt2img(id_task: [str], prompt: [str], negative_prompt: [str], prompt_
 def txt2img(id_task: str, prompt: str, negative_prompt: str, prompt_styles, steps: int, sampler_name: str, n_iter: int, batch_size: int, cfg_scale: float, height: int, width: int, enable_hr: bool, denoising_strength: float, hr_scale: float, hr_upscaler: str, hr_second_pass_steps: int, hr_resize_x: int, hr_resize_y: int, hr_checkpoint_name: str, hr_sampler_name: str, hr_prompt: str, hr_negative_prompt, override_settings_texts, request: gr.Request, *args):
     with open('account.txt', 'r') as f:
         accounts = f.readlines()
-    current_account = [item if request.username in item else None for item in accounts][0]
+
+    current_account = ""
+    for account in accounts:
+        if request.username in account:
+            current_account = account
+            break
+
+    
     user_name, pass_word, expired_date, ip = current_account.rstrip().split(':')
 
     if ip != request.client.host:
